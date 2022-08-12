4086d074bdb334c00f4a99aa801b70e02bb9a527
Evgeny Polyak <epolyak@luxoft.com>
2022-08-12 16:35:15 +0300

    Initial commit

---
+++ src/main/java/ml/pevgen/springwebawsexample/SpringWebAwsExampleApplication.java
@@ -0,0 +1,13 @@
+package ml.pevgen.springwebawsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAwsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebAwsExampleApplication.class, args);
+    }
+
+}
